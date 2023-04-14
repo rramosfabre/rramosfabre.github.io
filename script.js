fb63cf8ebdb8843aa8e6422429727a1dc07d9b37
rramosfabre <114057208+rramosfabre@users.noreply.github.com>
2023-04-14 14:36:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i;
+  var tabcontent = document.querySelectorAll(".tabcontent");
+  var tablinks = document.querySelectorAll(".tab");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+
+  if (!evt.currentTarget.classList.contains("active")) {
+    evt.currentTarget.classList.add("active");
+  }
+}
