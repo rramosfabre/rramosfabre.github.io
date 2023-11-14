3de1b7495b871e9db3fc18a9f249594160549ded
rramosfabre <114057208+rramosfabre@users.noreply.github.com>
2023-11-14 21:19:23 +0100

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function openTab(evt, tabName) {
-  // Oculta todos los contenidos de las pestañas
-  var tabcontents = document.querySelectorAll(".tabcontent");
-  tabcontents.forEach(tab => {
+  document.querySelectorAll(".tabcontent").forEach(tab => {
     tab.style.display = "none";
   });
 
-  // Desactiva todas las pestañas
-  var tablinks = document.querySelectorAll(".tab");
-  tablinks.forEach(tab => {
+  document.querySelectorAll(".tab").forEach(tab => {
     tab.classList.remove("active");
   });
 
-  // Muestra el contenido de la pestaña seleccionada
   document.getElementById(tabName).style.display = "block";
-
-  // Activa la pestaña seleccionada
   evt.currentTarget.classList.add("active");
 }
