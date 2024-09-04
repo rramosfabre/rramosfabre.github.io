99a1e611a41ce9b6f9ba9cd0e774a7987136c3e9
rramosfabre <114057208+rramosfabre@users.noreply.github.com>
2024-09-04 18:03:21 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Lista de enlaces
 const links = [
     { name: 'GitHub', icon: 'github', url: 'https://github.com/rramosfabre' },
     { name: 'LinkedIn', icon: 'linkedin', url: 'https://www.linkedin.com/in/ramón-o-ramos-fabré/' },
@@ -6,26 +7,30 @@ const links = [
     { name: 'Email', icon: 'mail', url: 'mailto:rramosfabre@protonmail.com' },
 ];
 
+// Función para crear enlaces
 function createLinkElements() {
     const linksContainer = document.getElementById('links-container');
     
     links.forEach(link => {
+        // Crear el elemento del enlace
         const linkElement = document.createElement('a');
         linkElement.href = link.url;
         linkElement.className = 'link';
-        linkElement.target = '_blank';
-        linkElement.rel = 'noopener noreferrer';
+        linkElement.target = '_blank'; // Abrir en una nueva pestaña
+        linkElement.rel = 'noopener noreferrer'; // Mejora de seguridad
         linkElement.setAttribute('aria-label', link.name);
         
+        // Insertar el contenido del enlace
         linkElement.innerHTML = `
             <span>${link.name}</span>
             <i data-feather="${link.icon}"></i>
         `;
         
+        // Agregar el enlace al contenedor
         linksContainer.appendChild(linkElement);
     });
     
-    // Reemplazar iconos de Feather
+    // Reemplazar los iconos de Feather
     feather.replace();
 }
 
